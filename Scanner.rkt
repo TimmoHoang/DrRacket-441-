91b8fb9b56216c2ea07c9c54c2ee2db552d04afa
Mondovall <60191886+Mondovall@users.noreply.github.com>
2021-10-13 07:40:15 -0500

    Add files via upload

--- Scanner.rkt
+++ Scanner.rkt
@@ -1,5 +1,28 @@
 #lang racket
+(require racket/trace)
 
-(define in(port->string(open-input-file "C:/Users/Timmy Neutron/Documents/Input01.txt")))
+
+
+(define (parse filepath)
+  (define in(string->list(port->string(open-input-file filepath))))
+  (scanner '() in "" 1)
+)
+
+(define (scanner lst str tempstr count)
+  (cond
+    [(empty? str) 0]
+    [(char-whitespace? (first str)) (scanner (cons lst tempstr) (rest str) "" count)]
+    
+    ;;[(equal? (first str) '#\( ) (scanner  lst (rest str) tempstr count)]
+    [(eqv? #\r (first str)) (scanner lst (rest str) tempstr (+ count 1))]
+    [else (scanner lst (rest str) (string-append tempstr (first str))  count)]
+  )
+  (display lst))
+    
+
+
+  
+            
+     
 
 
